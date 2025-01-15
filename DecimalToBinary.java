eae70c495868cec35cc12c7cddc5845edb869be1
Rin-ui <rajashri.biology@gmail.com>
2025-01-15 12:17:20 +0530

    DecimalToBinary.java

---
+++ DecimalToBinary.java
@@ -0,0 +1,7 @@
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        int decimalNum = 10;
+        String binaryNum = Integer.toBinaryString(decimalNum);
+        System.out.println("Decimal: " + decimalNum + " => Binary: " + binaryNum);
+    }
+}
